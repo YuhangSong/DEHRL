5e2a96fbeae12aa41640ccc91634e57f368a264c
YuhangSong <1120582412@qq.com>
2018-07-09 22:55:09 +0800

    add log behavior interval option

--- arguments.py
+++ arguments.py
@@ -86,6 +86,10 @@ def get_args():
     parser.add_argument('--reward-bounty', type=float,
                         help='the discount for the reward bounty, it would be different for shared_policy and hierarchical_policy' )
 
+    '''for log behavior'''
+    parser.add_argument('--log-behavior-interval', type=int, default=10,
+                        help='log behavior every x minutes')
+
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
