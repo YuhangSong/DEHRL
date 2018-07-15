51ff9538311e8bd883709b2352fa0df2fa917b81
YuhangSong <1120582412@qq.com>
2018-07-16 00:22:02 +0800

    change vis

--- arguments.py
+++ arguments.py
@@ -49,7 +49,7 @@ def get_args():
                         help='log interval, one log per n updates')
     parser.add_argument('--save-interval', type=int, default=100,
                         help='save interval, one save per n updates')
-    parser.add_argument('--vis-interval', type=int, default=1,
+    parser.add_argument('--vis-interval', type=int, default=10,
                         help='vis interval, one log per n updates')
     parser.add_argument('--num-frames', type=int, default=10e7,
                         help='number of frames to train (default: 10e6)')
